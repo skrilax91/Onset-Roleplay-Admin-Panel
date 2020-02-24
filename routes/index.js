6623bffe5a9194a1406f3e4f54df974d53ce1f25
Léo Brossard <leo.skrilax91@gmail.com>
2020-02-24 12:47:37 +0100

    Implement adding and deleting ban fonctionnality

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var { maintenance } = require('./views/maintenance');
 var { index } = require('./views/index');
 var { login, login_verify, logout } = require('./views/login');
 var { players_index} = require('./views/players/index');
-var { bans_index} = require('./views/bans/index');
+var { bans_index, bans_add, bans_remove} = require('./views/bans/index');
 
 // Homes pages
 router.get( '/', permissions(1), catchErrors( index ) );
@@ -20,6 +20,10 @@ router.get( '/logout/', logout );
 
 // players pages
 router.get( '/players/', permissions(1), catchErrors( players_index ));
+
+// bans pages
 router.get( '/bans/', permissions(1), catchErrors( bans_index ));
+router.post( '/bans/add/', permissions(1), catchErrors( bans_add ));
+router.get( '/bans/remove/:id', permissions(1), catchErrors( bans_remove ));
 
 module.exports = router;
--- routes/views/bans/index.js
+++ routes/views/bans/index.js
@@ -16,3 +16,92 @@ module.exports.bans_index = async ( req, res, next ) => {
 
     
 };
+
+module.exports.bans_remove = async ( req, res, next ) => {
+    id = req.params.id;
+    var ban = await new Promise( ( resolve, reject ) => {
+        database.query( 'SELECT * FROM bans WHERE id = ?', [ id ], ( err, results ) => {
+            if( err ){ reject(); throw err };
+
+            resolve( results[0] || {} );
+        })
+    });
+
+    if( ban.length < 1 ){
+        req.flash('error', "Ce bannissement n'existe pas !");
+        res.redirect('/bans/')
+        return;
+    }
+
+    database.query('DELETE FROM bans WHERE id = ?', [id] );
+    req.flash('success', "Ban révoqué");
+    res.redirect('/bans/');
+
+    
+};
+
+module.exports.bans_add = async ( req, res, next ) => {
+    var steamid = req.body.steamid;
+    var time = req.body.time;
+    var reason = req.body.reason;
+
+    if( !app.validPost( steamid ) ){
+        req.flash('danger', "Steamid invalide.");
+        res.reload();
+        return;
+    }
+
+    if( !app.validPost( time ) ){
+        time = "0";
+    }
+
+    if(reason.length > 128 ){
+        req.flash('danger', "La raison ne doit pas dépasser 128 lettres");
+        res.reload();
+        return;
+    }
+
+    var ban = await new Promise( ( resolve, reject ) => {
+        database.query( 'SELECT * FROM bans WHERE steamid = ?', [ steamid ], ( err, results ) => {
+            if( err ){ reject(); throw err };
+
+            resolve( results[0] || {} );
+        })
+    });
+
+    if( ban.length > 0 ){
+        req.flash('error', "Ce steamID est déjà banni !");
+        res.reload();
+        return;
+    }
+
+    database.query('INSERT INTO bans(steamid, ban_time, reason) VALUES(?, ?, ?)', [steamid, time, reason], ( err, result ) => {
+        if( err ) throw err;
+        req.flash('success', "ban créé créer avec succès.");
+        res.reload();
+    })
+
+};
+
+module.exports.bans_remove = async ( req, res, next ) => {
+    id = req.params.id;
+    var ban = await new Promise( ( resolve, reject ) => {
+        database.query( 'SELECT * FROM bans WHERE id = ?', [ id ], ( err, results ) => {
+            if( err ){ reject(); throw err };
+
+            resolve( results[0] || {} );
+        })
+    });
+
+    if( ban.length < 1 ){
+        req.flash('error', "Ce bannissement n'existe pas !");
+        res.reload();
+        return;
+    }
+
+    database.query('DELETE FROM bans WHERE id = ?', [id] );
+    req.flash('success', "Ban révoqué");
+    res.reload();
+
+    
+};
